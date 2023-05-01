12628bd679304fdf0b133c4c66c4a030f1dc61b8
warisu <abdullahiabdulwaris@gmail.comgit config --global user.email abdullahiabdulwaris@gmail.com>
2023-05-01 14:51:36 +0100

    Updates

---
+++ precache-manifest.5f484da070bdba633144ad0a1185f989.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2d79bb48caf65c62576",
+    "url": "/reactProject/static/css/main.64264234.chunk.css"
+  },
+  {
+    "revision": "a2d79bb48caf65c62576",
+    "url": "/reactProject/static/js/main.922162e4.chunk.js"
+  },
+  {
+    "revision": "8634270e03219b2f15e0",
+    "url": "/reactProject/static/js/runtime~main.76ed094f.js"
+  },
+  {
+    "revision": "09869a65efad09052e4a",
+    "url": "/reactProject/static/js/2.24a58385.chunk.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/reactProject/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/reactProject/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/reactProject/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/reactProject/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/reactProject/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/reactProject/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/reactProject/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/reactProject/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/reactProject/static/media/possibility.3c84ae26.png"
+  },
+  {
+    "revision": "21631c711ad6a0dc5883773f05b8863e",
+    "url": "/reactProject/index.html"
+  }
+];
